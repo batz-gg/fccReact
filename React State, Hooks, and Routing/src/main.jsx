43f03289d336f86c456cbc66a9ec65bcef7bd08b
batz-gg <batz.gg@gmail.com>
2025-11-03 15:56:04 +0800

    fruitsSearch app

--- React State, Hooks, and Routing/src/main.jsx
+++ React State, Hooks, and Routing/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ToggleApp } from './toggleText.jsx'
 import { ColorPicker } from './colorPicker.jsx'
+import { FruitsSearch } from './fruitsSearch.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToggleApp />
     <ColorPicker />
+    <FruitsSearch />
   </StrictMode>,
 )
